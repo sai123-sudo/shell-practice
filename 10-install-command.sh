c40a1639cc70fe44bc6f8d941a880209bb1caee8
sai123-sudo <ls0122207@gmail.com>
2025-05-20 05:40:32 +0530

    special vars

---
+++ 10-install-command.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USEERID -ne 0 ]
+then 
+    echo "ERROR:: Please run this script with root access"
+else  
+    echo "You are running with root access"
+fi  
