6966516f60b65ce562c5a01390ef5964f5400179
sai123-sudo <ls0122207@gmail.com>
2025-05-25 12:50:34 +0530

    colors

--- 12-colors.sh
+++ 12-colors.sh
@@ -4,4 +4,4 @@ echo -e "\e[35m em jarigina parledhu chooskundham \e[0m"
 
 echo "ane vaadu yeppudu antune vuntadu"
 
-echo -e "\e[31m yenni kastalu vacchina sare \e[33m DevOps \e[0m madyalo aga kuudathu \e0m"
+echo -e "\e[31m yenni kastalu vacchina sare \e[33m DevOps \e[0m madyalo aga kuudathu \e[0m"
